c86e28d8c82c46a1a254f0ea9d07b5428db54dad
Kamil <szpmytyl@wp.pl>
2025-07-01 13:33:44 +0200

    🔒 Ukryto dane logowania w pliku .env

--- Desktop/node-express-notes-api/db.js
+++ Desktop/node-express-notes-api/db.js
@@ -1,14 +1,18 @@
+require("dotenv").config();
 const { MongoClient } = require("mongodb");
 
-const uri =
-  "mongodb+srv://kamil:Programista2025@cluster0.bssbwly.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.bssbwly.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority&appName=Cluster0`;
+
 const client = new MongoClient(uri);
-const dbName = "notesDB";
 
 async function connectDB() {
-  await client.connect();
-  console.log("✅ Połączono z MongoDB Atlas");
-  return client.db(dbName).collection("notes");
+  try {
+    await client.connect();
+    console.log("✅ Połączono z MongoDB");
+    return client.db(process.env.MONGO_DB).collection("notes");
+  } catch (error) {
+    console.error("❌ Błąd połączenia z MongoDB:", error);
+  }
 }
 
 module.exports = connectDB;
